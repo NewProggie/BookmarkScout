0014fcb336ef0b4f5504b6ef784887e94ada403a
Kai Wolf <kai.wolf@gmail.com>
2012-02-22 19:06:21 +0100

    Very basic user authentification (working only in development mode)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
@@ -6,9 +7,14 @@ from google.appengine.ext.webapp import template
 class MainPage(webapp.RequestHandler):
 
 	def get(self):
-		template_values = {'greeting': 'Hallo, BookmarkScout!' }
-		path = os.path.join(os.path.dirname(__file__), 'index.html')
-		self.response.out.write(template.render(path, template_values))
+		user = users.get_current_user()
+		if user:
+			template_values = { 'user': user.nickname(), 
+								'logout_url': users.create_logout_url('http://localhost:8080') }
+			path = os.path.join(os.path.dirname(__file__), 'index.html')
+			self.response.out.write(template.render(path, template_values))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp.WSGIApplication(
 									[('/', MainPage)],

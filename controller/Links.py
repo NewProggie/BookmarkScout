0c8423cc175a491657d45318c63ef283076cc656
Kai Wolf <kai.wolf@gmail.com>
2012-02-27 23:26:58 +0100

    Added tags to view and some layout adjustments - Included tags into view - Created random function for creating new tags - switched from list view to divs for displaying each saved link

--- controller/Links.py
+++ controller/Links.py
@@ -2,18 +2,26 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 
 from model.Link import Link
-
+import random # just for debugging purpose
 
 class Links(webapp.RequestHandler):
-	""" Class Links """
+    """ Class Links """
 
-	def post(self):
-		link = Link()
-		if users.get_current_user():
-			link.user = users.get_current_user()
-			link.url = self.request.get('url')
-			link.tags = []
-			link.notes = self.request.get('notes')
-			link.is_privat = True if self.request.get('is_privat') else False
-			link.put()
-			self.redirect('/')
+    def get_tags(self, amount):
+        """ Just for debugging purposes
+        """
+        tags = []
+        for i in range(amount):
+            tags.append(random.choice(['foo', 'bar', 'else', 'choice', 'what' ,'lolcats', 'ponies', 'cunts']))
+        return tags
+        
+    def post(self):
+        link = Link()
+        if users.get_current_user():
+            link.user = users.get_current_user()
+            link.url = self.request.get('url')
+            link.tags = self.get_tags(random.randint(1,7))
+            link.notes = self.request.get('notes')
+            link.is_privat = True if self.request.get('is_privat') else False
+            link.put()
+            self.redirect('/')
